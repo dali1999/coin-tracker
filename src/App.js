14a89e16e1712953eb607f139cb37ef4bad3bf06
Jules <dali1999@naver.com>
2023-08-25 15:53:29 +0900

    coin API 가져온 후 나열

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      }); //api 받아오기
+  }, []); //빈 배열이므로 이 코드는 한번만 작동
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>The Coins!({coins.length})</h1>
+      {loading ? <strong>Loading...</strong> : null}
+      <select key={coins.id}>
+        {coins.map((coin, index) => (
+          <option>
+            {coin.name} ({coin.symbol}): $
+            {coin.hasOwnProperty("quotes")
+              ? coin.quotes.hasOwnProperty("USD")
+                ? coin.quotes.USD.price
+                : null
+              : null}{" "}
+            USD
+          </option>
+        ))}
+      </select>
     </div>
   );
 }
