d64050a6e37e6dbcc175bdd70a4e4ad7d5e26849
Jules <dali1999@naver.com>
2023-08-25 16:33:43 +0900

    액수 입력 칸 추가

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { useEffect, useState } from "react";
 function App() {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
+  const [money, setMoney] = useState(0);
+  const onChange = (event) => setMoney(event.target.value);
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
       .then((response) => response.json())
@@ -13,21 +15,32 @@ function App() {
   }, []); //빈 배열이므로 이 코드는 한번만 작동
   return (
     <div>
-      <h1>The Coins!({coins.length})</h1>
-      {loading ? <strong>Loading...</strong> : null}
-      <select key={coins.id}>
-        {coins.map((coin, index) => (
-          <option>
-            {coin.name} ({coin.symbol}): $
-            {coin.hasOwnProperty("quotes")
-              ? coin.quotes.hasOwnProperty("USD")
-                ? coin.quotes.USD.price
-                : null
-              : null}{" "}
-            USD
-          </option>
-        ))}
-      </select>
+      <h1>The Coins!{loading ? "" : `(${coins.length})`}</h1>
+
+      <input
+        onChange={onChange}
+        type="number"
+        placeholder="How much do you have?"
+        value={money}
+      />
+
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select>
+          {coins.map((coin, index) => (
+            <option>
+              {coin.name}({coin.symbol}): $
+              {coin.hasOwnProperty("quotes")
+                ? coin.quotes.hasOwnProperty("USD")
+                  ? coin.quotes.USD.price
+                  : null
+                : null}{" "}
+              USD{", "}You can have {money / coin.quotes.USD.price} BTC!
+            </option>
+          ))}
+        </select>
+      )}
     </div>
   );
 }
